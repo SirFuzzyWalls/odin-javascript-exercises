68bdc2b42a0dc9f888b67b67f5ed64578094dbb8
SirFuzzyWalls <alexzandermcgraw@gmail.com>
2024-10-23 14:53:23 -0700

    done: power func, todo: multiply func

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -6,16 +6,24 @@ const subtract = function(a, b) {
   return a - b;
 };
 
-const sum = function(a,b) {
-	return a + b;
+const sum = function(arr) {
+  summed = arr.reduce((sum, curr) => sum + curr, 0);
+  return summed;
 };
 
-const multiply = function() {
-
+const multiply = function(...args) {
+  for( let i = 0; i < args.length; i++){
+    if(i == 0){
+      let product = args[i];
+    } else {
+      product *= args[i];
+    }
+  }
+  return product;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
 const factorial = function() {
