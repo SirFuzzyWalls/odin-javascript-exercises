51ca3c6c38e7223673fe5d432b0a1f9df0ccd3cc
SirFuzzyWalls <alexzandermcgraw@gmail.com>
2024-10-24 17:28:27 -0700

    palindrome checker completed

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    const alphanumerical = 'abcdefghijklmnopqrstuvwxyz0123456789';
+    const cleanStr = str
+        .toLowerCase()
+        .split('')
+        .filter((char) => alphanumerical.includes(char))
+        .join('');
+    const reversedStr = cleanStr.split('').reverse().join('');
+    return cleanStr === reversedStr;
 };
 
 // Do not edit below this line
