bcd318fdbfd081453b497aed4efc8437e4177b88
SirFuzzyWalls <alexzandermcgraw@gmail.com>
2024-10-30 14:49:29 -0700

    all tests complete

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,9 @@
 const findTheOldest = function(arr) {
     const sortedByAge = arr.slice(0).sort((a,b) => {
-        return (b.yearOfDeath - b.yearOfBirth) - (a.yearOfDeath - a.yearOfBirth);
+        const currentYear = new Date().getFullYear();
+        const ageA = (a.yearOfDeath || currentYear) - a.yearOfBirth;
+        const ageB = (b.yearOfDeath || currentYear) - b.yearOfBirth;
+        return ageB - ageA;
     })
     return sortedByAge[0];
 };
